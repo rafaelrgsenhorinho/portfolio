44fa084c171ce9f0a0257ddfca630eca03e1986c
Rafael <rafaelrgsenhorinho@gmail.com>
2024-10-18 16:06:16 -0300

    update code and portfolio fix 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,6 +46,7 @@ function updateContent(data, language) {
 
 // Initialize the page with the default language
 async function initializePage() {
+    const data = await fetchTranslateData();
     const defaultLanguage = 'en';
     updateContent(data, defaultLanguage);
     document.getElementById('translate-btn').textContent = defaultLanguage === 'en' ? 'EN' : 'PT';
@@ -63,65 +64,5 @@ document.getElementById('translate-btn').addEventListener('click', async () => {
 }    
 });
 
-async function initializeWorks(data, language) {
-    const worksData = data[language]['work-element']; // Adjust according to your JSON structure
-    displayWorks(worksData, language);
-}
-
-function displayWorks(data, language) {
-    const content = data[language];
-    const worksContainer = document.getElementById('mainWorks');
-
-    content.forEach(content => {
-        const workElement = document.createElement('div');
-        workElement.classList.add('work-element');
-        
-        workElement.innerHTML = `
-            <span class="work-name">${content.name}</span>
-            <div class="work-content">
-                <div class="content-container">
-                    <picture>
-                        <img src="${content.image}" alt="Image of ${content.name}">
-                    </picture>
-                    <div class="work-description">
-                        <div class="title-buttons">
-                            <h4>${content.name}</h4>
-                            <div class="buttons-container">
-                                <a href="${content.links.github}" target="_blank">
-                                    <button class="btn-work">GitHub Project</button>
-                                </a>
-                                <a href="${content.links.live}" target="_blank">
-                                    <button class="btn-work">Live Project</button>
-                                </a>
-                            </div>
-                        </div>
-                        <p>${content.description}</p>
-                    </div>
-                    <div class="icons-container">
-                        ${content.tech.map(icon => `
-                            <picture>
-                                <img src="./assets/images/icons/${icon}" alt="${icon}">
-                            </picture>
-                        `).join('')}
-                    </div>
-                </div>
-            </div>
-        `;
-        
-        worksContainer.appendChild(workElement);
-    });
-}
-
-
-
-
 // Initialize the page content when the DOM is fully loaded
-document.addEventListener('DOMContentLoaded', async function() {
-    try {
-        const data = await fetchTranslateData(); // Fetch data here
-        initializePage(data); // Pass data to initializePage
-        initializeWorks(data, 'en'); // Initialize works with default language
-    } catch (error) {
-        console.error('Error fetching translation data:', error);
-    }
-});
+document.addEventListener('DOMContentLoaded', initializePage);
